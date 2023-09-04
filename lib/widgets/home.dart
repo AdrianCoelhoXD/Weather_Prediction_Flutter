852a786be1a18cb055893795c16ca5c8e4dcf21e
AdrianCoelhoXD <adriantwocoelho2010@outlook.com>
2023-09-04 19:04:03 -0300

    Criação do resumo

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weathe_application/widgets/resumo.dart';
 
 class home extends StatefulWidget {
   const home({super.key});
@@ -11,6 +12,23 @@ class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Prevision weather"), centerTitle: true));
+      appBar: AppBar(
+        title: Text("Prevision weather"),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          Resumo(
+            cidade: 'Campina Grande-PB',
+            temperaturaAtual: 33,
+            temperaturaMaxima: 39,
+            temperaturaMinima: 27,
+            descricao: 'Ensolarado',
+            numeroIcone: 1,
+          ),
+        ],
+      )),
+    );
   }
 }
